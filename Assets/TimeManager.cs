9b8aaf5971dbb335b6bc6a2b483442ac38c310b2
Yuki <>
2020-09-16 12:19:33 +0900

    up

--- Assets/TimeManager.cs
+++ Assets/TimeManager.cs
@@ -21,6 +21,12 @@ public class TimeManager : MonoBehaviour
     [SerializeField]
     int ones_place=0;
 
+    [SerializeField]
+    int[] place = new int[4];
+    int placeX;
+    
+    int[] s = { 600,60,10,1};
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,19 +35,29 @@ public class TimeManager : MonoBehaviour
             
             var tr = transform.GetChild(hoge).GetComponent<Timer>();
             time[hoge] = tr;
+            
         }
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        countup += Time.deltaTime*100;
+        countup += Time.deltaTime;
+        place[0] = (int)countup / s[0];
 
-        thousand_place= (int)countup / 600;
-        hundreds_place = (int)(countup-(thousand_place * 600)) / 60;
-        tens_place = (int)(countup - (thousand_place * 600)-hundreds_place*60) / 10;
-        ones_place= (int)countup- (thousand_place * 600)-(hundreds_place * 60)- (tens_place * 10);
+        for (int i = 1; i < 4; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                placeX -= place[j] * s[j];
+                //Debug.Log("j="+j+","+"i="+i);
+            
+            }
+            Debug.Log(((int)countup - placeX)+","+s[i]);
+            place[i] = ((int)countup - placeX) / s[i];
+
+            placeX = 0;
+        }
 
 
 
@@ -50,3 +66,9 @@ public class TimeManager : MonoBehaviour
 
     }
 }
+/*
+ thousand_place= (int)countup / 600;
+        hundreds_place = (int)(countup-(thousand_place * 600)) / 60;
+        tens_place = (int)(countup - (thousand_place * 600)-hundreds_place*60) / 10;
+        ones_place= (int)countup- (thousand_place * 600)-(hundreds_place * 60)- (tens_place * 10);
+ */
